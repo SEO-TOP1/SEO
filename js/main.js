68933b2bcd5695c686f3db045c305206259b275d
Nguyen Huu Phat <huuphat1908@gmail.com>
2021-05-21 08:25:33 +0700

    Archive img in index page

--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
-/*  ---------------------------------------------------
-    Template Name: Fashi
-    Description: Fashi eCommerce HTML Template
-    Author: Colorlib
-    Author URI: https://colorlib.com/
-    Version: 1.0
-    Created: Colorlib
----------------------------------------------------------  */
-
 'use strict';
 
 (function ($) {
-
-    /*------------------
-        Preloader
-    --------------------*/
-    $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
-    });
-
     /*------------------
         Background Set
     --------------------*/
